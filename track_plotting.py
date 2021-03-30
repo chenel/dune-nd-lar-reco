3a416bae2a6018c01bcadbf3c78204010259afc9
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2021-03-30 08:25:43 -0400

    get track units right; cosmetic stuff

--- track_plotting.py
+++ track_plotting.py
@@ -9,47 +9,65 @@ TRACK_LABEL = 1
 
 TRACK_THRESHOLD = 5   # voxels.  will be converted to geometry units below
 
-def convert_pixel_to_geom(val, data):
+
+def convert_pixel_to_geom(val, metadata):
 	# assume cubical, for now...
-	return val * data["metadata"].size_voxel_x()
+	return val * metadata.size_voxel_x()  # note these will be in cm
 
 
-def track_lengths(vals):
-	track_indices = numpy.unique(vals["track_group_pred"])
-	lengths = numpy.full((len(track_indices)), numpy.nan)
-	for i, trk_index in enumerate(track_indices):
-		voxel_indices = numpy.concatenate([frag for idx, frag in enumerate(vals["track_fragments"]) if vals["track_group_pred"][idx] == trk_index])
-		voxels = vals["input_data"][voxel_indices][:, :3]
-		lengths[i] = numpy.max(scipy.spatial.distance.cdist(voxels, voxels))
+def reco_track_lengths_cm(vals):
+	""" return the lengths of all reco track clusters in units of cm """
+	if "reco_track_lengths" in vals:
+		lengths = vals["reco_track_lengths"]
+	else:
+		track_indices = numpy.unique(vals["track_group_pred"])
+		lengths = numpy.full((len(track_indices)), numpy.nan)
+		for i, trk_index in enumerate(track_indices):
+			voxel_indices = numpy.concatenate([frag for idx, frag in enumerate(vals["track_fragments"]) if vals["track_group_pred"][idx] == trk_index])
+			voxels = vals["input_data"][voxel_indices][:, :3]
+			lengths[i] = numpy.max(scipy.spatial.distance.cdist(voxels, voxels))
+		vals["reco_track_lengths"] = lengths   # store so if called again for this event we won't have to recalculate
 
 	return lengths
 
 
+def truth_track_lengths_vox(vals):
+	""" return the lengths of all true tracks in units of voxels """
+	if "true_track_lengths" in vals:
+		lengths = vals["true_track_lengths"]
+	else:
+		lengths = numpy.array([p.end_position().as_point3d().distance(p.position().as_point3d())
+		                       for p in vals["particles"]
+		                       if p.shape() == TRACK_LABEL])
+		vals["true_track_lengths"] = lengths
+
+	return lengths
+
+
+
+
 @plotting_helpers.hist_aggregate("n-tracks-reco", bins=15, range=(0,15))
 def agg_ntracks_reco(vals):
-	lengths = track_lengths(vals)
-	length_mask = lengths > convert_pixel_to_geom(TRACK_THRESHOLD, vals)
+	lengths = reco_track_lengths_cm(vals)
+	length_mask = lengths > convert_pixel_to_geom(TRACK_THRESHOLD, vals["metadata"])
 	return [numpy.count_nonzero(length_mask),]
 
 
 @plotting_helpers.hist_aggregate("n-tracks-true", bins=15, range=(0,15))
 def agg_ntracks_true(vals):
 	# here the track threshold is used in voxels because that's what the internal representation of the particles is
-	return [len([p for p in vals["particles"]
-	             if p.shape() == TRACK_LABEL
-	             and p.end_position().as_point3d().distance(p.position().as_point3d()) > TRACK_THRESHOLD]),]
+	lengths = truth_track_lengths_vox(vals)
+	return numpy.count_nonzero(lengths > TRACK_THRESHOLD)
 
 
-@plotting_helpers.hist_aggregate("trk-length-true", bins=numpy.logspace(0, numpy.log10(300), 50))
+@plotting_helpers.hist_aggregate("trk-length-true", bins=numpy.logspace(-1, numpy.log10(3000), 50))
 def agg_trklen_true(vals):
-	return [p.end_position().as_point3d().distance(p.position().as_point3d()) * 0.1  # convert to cm
-	        for p in vals["particles"]
-	        if p.shape() == TRACK_LABEL]
+	return convert_pixel_to_geom(truth_track_lengths_vox(vals), vals["metadata"])
 
 
-@plotting_helpers.hist_aggregate("trk-length-reco", bins=numpy.logspace(0, numpy.log10(300), 50))
+@plotting_helpers.hist_aggregate("trk-length-reco", bins=numpy.logspace(-1, numpy.log10(3000), 50))
 def agg_trklen_reco(vals):
-	return track_lengths(vals)
+	return reco_track_lengths_cm(vals)
 
 
 #------------------------------------------------------
@@ -57,7 +75,6 @@ def agg_trklen_reco(vals):
 @plotting_helpers.req_vars_hist(["input_data", "track_fragments", "track_group_pred", "particles", "metadata"])
 def BuildHists(data, hists):
 	for evt_idx in range(len(data["particles"])):
-		print("Evaluating event #", evt_idx)
 		# first: number of tracks
 		evt_data = { k: data[k][evt_idx] for k in data }
 		for agg_fn in (agg_trklen_reco, agg_trklen_true, agg_ntracks_reco, agg_ntracks_true):
@@ -73,7 +90,7 @@ def PlotHists(hists, outdir, fmts):
 	ntracks_hists = {hname: hists[hname] for hname in ("n-tracks-reco", "n-tracks-true")}
 	if all(ntracks_hists.values()):
 		fig, ax = plotting_helpers.overlay_hists(ntracks_hists,
-		                                         xaxis_label="'Track' cluster multiplicity",
+		                                         xaxis_label="'Track' cluster multiplicity (length > %d voxels)" % TRACK_THRESHOLD,
 		                                         hist_labels={"n-tracks-reco": "Reco", "n-tracks-true": "True"})
 
 		plotting_helpers.savefig(fig, "n-tracks", outdir, fmts)
@@ -82,5 +99,7 @@ def PlotHists(hists, outdir, fmts):
 	if all(trklen_hists.values()):
 		fig, ax = plotting_helpers.overlay_hists(trklen_hists,
 		                                         xaxis_label="'Track' length (cm)",
+		                                         yaxis_label="Tracks",
 		                                         hist_labels={"trk-length-reco": "Reco", "trk-length-true": "True"})
+		ax.set_xscale("log")
 		plotting_helpers.savefig(fig, "trk-len", outdir, fmts)
