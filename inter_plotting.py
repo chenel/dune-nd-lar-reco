573b3436500919952915826e2921629dbc2fe856
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2021-07-26 17:08:36 -0400

    plots about true muons in interactions

--- inter_plotting.py
+++ inter_plotting.py
@@ -1,5 +1,6 @@
 import copy
 
+import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.colors
 import numpy
@@ -7,14 +8,29 @@ import scipy.spatial
 
 import plotting_helpers
 
+# a muon is considered "split" if
+# more than this fraction of its energy
+# is found in multiple reco interactions
+TRUE_MUON_SPLIT_FRAC = 0.2
+
+# the threshold for considering a reco interaction
+# to contain a "significant" part of a muon
+# when counting interactions that merge muons
+TRUE_MUON_MERGE_FRAC = 0.2
+
 VOXEL_THRESHOLD_MEV = 0.25
 
 
 def find_matching_rows(a1, a2, mask_only=False):
 	""" Find the indices of rows of 2D array a1 that match to rows of 2D array a2
 	"""
-	assert(a1.shape[1] == a2.shape[1])
 	assert(a1.dtype == a2.dtype)
+
+	# the 1D case is straightforward
+	if len(a1.shape) == 1:
+		return np.argwhere(np.isin(a1, a2))
+
+	assert(a1.shape[1] == a2.shape[1])
 
 	# adapted from https://stackoverflow.com/questions/16210738/implementation-of-numpy-in1d-for-2d-arrays
 	dtype = ",".join([str(a1.dtype),] * a1.shape[1])
@@ -37,6 +53,10 @@ def find_matching_rows(a1, a2, mask_only=False):
 	# return numpy.argwhere(matches_by_a2row)[:, 1]
 
 def true_inter_lbls(vals):
+	""" Get the list of true interaction labels.
+
+		:return: 1D numpy array of interaction labels (ints)
+	"""
 	if "true_inter_ids" not in vals:
 		vals["true_inter_ids"] = numpy.unique(vals["cluster_label"][:, 7])
 
@@ -44,6 +64,10 @@ def true_inter_lbls(vals):
 
 
 def true_inter_voxel_ids(vals, inter_lbl):
+	"""  Get a list of the indices of voxels in "input_data" that match to the given true interaction label.
+
+		:param inter_lbl   The true interaction label of interest
+		:return: array of indices for voxels in vals["input_data"] """
 	if "true_inter_voxel_ids" not in vals:
 		vals["true_inter_voxel_ids"] = {}
 	if inter_lbl not in vals["true_inter_voxel_ids"]:
@@ -53,10 +77,80 @@ def true_inter_voxel_ids(vals, inter_lbl):
 
 	return vals["true_inter_voxel_ids"][inter_lbl]
 
-def true_reco_inter_match(vals):
-	pass
+
+def true_muon_reco_matches(vals):
+	"""
+	Build a table of matches from each true muon in the spill to the reco interactions
+	that contain some of its energy (and store the fraction of the true muon energy's energy matched).
+
+	:return: dict of dicts { true muon cluster id: { reco interaction label : fraction of true mu energy } }
+	"""
+	key = "true_muon_recoint_matches"
+
+	if key not in vals:
+		true_mu_recoint_match = {}
+		for mu_cluster_id, vox_idxs in true_muon_voxidxs_by_cluster(vals).items():
+			total_mu_visE = vals["input_data"][vox_idxs, 4].sum()
+			assert total_mu_visE > 0
+			print("muon label", mu_cluster_id, " voxels:", vox_idxs)
+			for reco_inter_label in numpy.unique(vals["inter_group_pred"]):
+				reco_inter_vox = reco_inter_voxel_ids(vals, reco_inter_label)
+#				print("   reco interaction", reco_inter_label, "voxels:", type(reco_inter_vox), reco_inter_vox)
+				matched_vox_idxs = find_matching_rows(vox_idxs, reco_inter_vox)
+				if len(matched_vox_idxs) > 0:
+					if mu_cluster_id not in true_mu_recoint_match:
+						true_mu_recoint_match[mu_cluster_id] = {}
+					true_mu_recoint_match[mu_cluster_id][reco_inter_label] = vals["input_data"][vox_idxs[matched_vox_idxs], 4].sum() / total_mu_visE
+
+		vals[key] = true_mu_recoint_match
+
+	print(vals[key])
+	return vals[key]
+
+def true_muon_voxidxs_by_cluster(vals):
+	"""
+	Build a map from all true muons in the spill to the indices of the voxels in the data.
+
+	:return:  dict of arrays of indices in vals["input_data"] corresponding to each muon, keyed by true muon cluster label
+	"""
+
+	# vals["cluster_label"][:, 8] is PDG code
+	key = "true_muon_vox_by_cluster"
+
+	if key not in vals:
+		# Both mu+ and mu- SHOULD get value "2" in column 9.
+		# Unfortunately this info is currently broken for reasons not yet known.
+		# (It only seems to be correctly filled for the first muon encountered in the spill.)
+		# So we have to bootstrap from the full particle list. :(
+		part_pdg_group = numpy.array([(p.pdg_code(), p.group_id()) for p in vals["particles_raw"]])
+		true_muon_groups = numpy.unique(part_pdg_group[abs(part_pdg_group[:, 0]) == 13][:, 1])
+
+
+		# column 5 is "cluster ID", which is the GEANT4 ID,
+		# but unfortunately, our current overlay files have GEANT4 interactions
+		# that were generated separately overlaid on each other.
+		# column 6 is "group ID", which is usable.
+		muon_vox = {}
+		for mu_id in true_muon_groups:
+			vox = find_matching_rows(numpy.array(vals["input_data"][:, :3]),
+			                                     numpy.array(vals["cluster_label"][vals["cluster_label"][:, 6] == mu_id, :3]))[0]
+			if len(vox) > 0:
+				muon_vox[mu_id] = vox
+
+		vals[key] = muon_vox
+
+#	print("returning from true_muon_voxidxs_by_cluster():", vals[key])
+	return vals[key]
+
 
 def reco_inter_voxel_ids(vals, inter_lbl):
+	"""
+	Obtain a list of the voxels corresponding to a reconstructed interaction.
+
+	:param inter_lbl:  reco interaction label of interest
+	:return: array of indices in vals["input_data"] corresponding to the voxels for that interaction
+	"""
+
 	# each entry in "inter_group_pred" is the interaction group id
 	# for the cluster group object in "inter_particles" at the same index,
 	# so we have to combine the voxels in all those cluster groups
@@ -74,6 +168,45 @@ def reco_inter_voxel_ids(vals, inter_lbl):
 
 
 #------------------------------------------------------
+
+@plotting_helpers.hist_aggregate("true-muon-reco-int-match-count", bins=10, range=(0, 10))
+def agg_true_muon_reco_int_match_count(vals):
+	"""
+	Count how many reco interactions contain at least  TRUE_MUON_SPLIT_FRAC of each true muon.
+	"""
+	true_muon_recoint_matches = true_muon_reco_matches(vals)
+
+	return [sum(1 if frac >= TRUE_MUON_SPLIT_FRAC else 0 for frac in mu_match_fracs.values())
+	        for mu_match_fracs in true_muon_recoint_matches.values()]
+
+
+@plotting_helpers.hist_aggregate("true-muon-grouped-frac", bins=27, range=(0, 1.08))
+def agg_true_muon_grouped_frac(vals):
+	"""
+	Compute the fraction of the true muon's energy contained by the reco interaction
+	that contains the most of its energy.
+	"""
+	true_muon_recoint_matches = true_muon_reco_matches(vals)
+
+	return [sum(mu_match_fracs.values()) for mu_match_fracs in true_muon_recoint_matches.values()]
+
+
+@plotting_helpers.hist_aggregate("reco-int-muon-match-count", bins=10, range=(0,10))
+def agg_reco_int_muon_match_count(vals):
+	"""
+	For each reco interaction, count the number of true muons
+	for which at least TRUE_MUON_MERGE_FRAC of their energy
+	is contained in that interaction.
+	"""
+	true_muon_recoint_matches = true_muon_reco_matches(vals)
+	reco_int_muon_count = {reco_int: 0 for reco_int in numpy.unique(vals["inter_group_pred"])}
+	for reco_int_fracs in true_muon_recoint_matches.values():
+		for reco_int, frac in reco_int_fracs.items():
+			if frac >= TRUE_MUON_MERGE_FRAC:
+				reco_int_muon_count[reco_int] += 1
+
+	return list(reco_int_muon_count.values())
+
 
 @plotting_helpers.hist_aggregate("n-inter-reco", bins=50, range=(0,100))
 def agg_ninter_reco(vals):
@@ -192,14 +325,15 @@ def agg_recoint_purity(vals):
 #------------------------------------------------------
 
 @plotting_helpers.req_vars_hist(["input_data", "inter_group_pred", "inter_particles", "cluster_label",
-                                 "metadata", "event_base"])
+                                 "metadata", "event_base", "particles_raw"])
 def BuildHists(data, hists):
 	for evt_idx in range(len(data["cluster_label"])):
 		# first: number of tracks
 		evt_data = { k: data[k][evt_idx] for k in data }
 		for agg_fn in (agg_ninter_reco, agg_ninter_true, agg_nvox_inter_reco, agg_nvox_inter_true,
 		               agg_ungrouped_trueint_energy_frac, agg_trueint_largest_matched_energy_frac,
-		               agg_recoint_purity):
+		               agg_recoint_purity, agg_true_muon_grouped_frac, agg_true_muon_reco_int_match_count,
+		               agg_reco_int_muon_match_count):
 			agg_fn(evt_data, hists)
 
 
@@ -228,7 +362,13 @@ def PlotHists(hists, outdir, fmts):
 		"largest-trueint-energy-matched-frac": (r"Max frac. true vis. $E_{dep}$ matched to reco int.",
 		                                        r"True $\nu$ interactions"),
 		"recoint-purity-frac":                 (r"Reco. interaction purity",
-		                                         r"Reco. $\nu$ interactions"),
+		                                        r"Reco. $\nu$ interactions"),
+		"true-muon-reco-int-match-count":      (r"Reco. ints. w/ $\geq{0:.0f}\%$ of true vis. $E_{{\mu}}$".format(TRUE_MUON_SPLIT_FRAC*100),
+		                                        "True muons"),
+		"true-muon-grouped-frac":              (r"'Leading' frac. of vis. $E_{\mu}$ matched to reco int.",
+	                                            "True muons"),
+		"reco-int-muon-match-count":           (r"Num. muons w/ $\geq{0:.0f}\%$ of true vis. $E_{{\mu}}$".format(TRUE_MUON_MERGE_FRAC*100),
+		                                        "Reco. interactions"),
 	}
 	for plot, (xlabel, ylabel) in hist_labels.items():
 		if plot in hists:
